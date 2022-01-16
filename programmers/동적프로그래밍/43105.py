f54d125dee7e7fc181e2472ad4a4614a6adfccef
Amebr <chaechae1214@naver.com>
2022-01-17 01:51:32 +0900

    정수 삼각형

---
+++ programmers/동적프로그래밍/43105.py
@@ -0,0 +1,13 @@
+def solution(triangle):
+
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][0]
+            elif i == j:
+                triangle[i][j] += triangle[i-1][-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+                
+    answer = max(triangle[-1])
+    return answer
